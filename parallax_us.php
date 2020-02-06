39cea7bd8696778a3694475c0f22065716a2db9a
GuigzSciencesU <60275318+GuigzSciencesU@users.noreply.github.com>
2020-02-06 20:09:00 +0100

    Update parallax_us.php

--- parallax_us.php
+++ parallax_us.php
@@ -1,1 +1,51 @@
 
+<!DOCTYPE html>
+
+<body>
+
+	<!-- Image 1 -->
+    <div class="parallax-container">
+      <div class="parallax"><img src="images/fond_01.jpg"></div>
+    </div>
+
+
+
+	<!-- A PROPOS -->
+    <div class="section white">
+      <div class="row container">
+        <h2 class="header">Sports</h2>
+        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+      </div>
+    </div>
+
+
+
+	<!-- Image 2 -->
+    <div class="parallax-container">
+      	<div class="parallax">
+      		<img src="images/Sport_03.jpg" alt="" class="responsive-img">
+		</div>
+    </div>
+    
+
+
+	<!-- SPORTS -->
+    <div class="section white">
+      <div class="row container">
+        <h2 class="header">Sports</h2>
+		<?php include "prlxB_us.php"  ?>
+        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+      </div>
+    </div>
+
+
+
+	<!-- Image 3 -->
+    <div class="parallax-container">
+      <div class="parallax"><img src="images/Sport_05.jpg"></div>
+    </div>
+
+
+</body>
+
+</html>
