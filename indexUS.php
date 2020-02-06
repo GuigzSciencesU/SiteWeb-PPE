04dc5300194dc86f07268cbe8d09600c1a259a5c
GuigzSciencesU <60275318+GuigzSciencesU@users.noreply.github.com>
2020-02-06 18:40:21 +0100

    Update indexUS.php

--- indexUS.php
+++ indexUS.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-  <html>
+<html>
+
+
+      <title>U'Sports</title>
+
 
 <!-- Import Materialize CSS / Icones -->
 <head>
+
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
+      <link type="text/css" rel="stylesheet" href="css/custom_us.css"  media="screen,projection"/>
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 </head>
 
 
@@ -26,7 +32,9 @@
       <?php include "footer_us.php" ?>
 
       <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/jquery.min.js"></script>      
       <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/custom_us.js"></script>
 
 </body>
 
