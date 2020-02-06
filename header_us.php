ec13be92c8ab75363b52006186aa362bb7f71ec1
GuigzSciencesU <60275318+GuigzSciencesU@users.noreply.github.com>
2020-02-06 20:09:25 +0100

    Update header_us.php

--- header_us.php
+++ header_us.php
@@ -1,1 +1,23 @@
 
+<!DOCTYPE html>
+<html>
+<body>
+
+
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">U'Sports</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Accueil</a></li>
+        <li><a href="badges.html">A propos</a></li>
+        <li><a href="collapsible.html">Sports</a></li>
+        <li><a href="collapsible.html">Planning</a></li>
+        <li><a href="collapsible.html">Inscriptions</a></li>      
+      </ul>
+    </div>
+  </nav>
+        
+
+
+</body>
+</html>
