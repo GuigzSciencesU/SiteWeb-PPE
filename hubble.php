58ad364db2e16d692a60a667a3772efd57e5e0f3
GuigzSciencesU <60275318+GuigzSciencesU@users.noreply.github.com>
2020-02-25 09:26:37 +0100

    Create hubble.php

---
+++ hubble.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html style="background-color: #000000;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>ATOM</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="/assets/css/Contact-Form-Clean.css">
+    <link rel="stylesheet" href="/assets/css/Features-Clean.css">
+    <link rel="stylesheet" href="/assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.css">
+    <link rel="stylesheet" href="/assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="/assets/css/Newsletter-Subscription-Form.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/Testimonials.css">
+    <link rel="stylesheet" href="/assets/css/untitled.css">
+</head>
+
+<body style="background-color: rgb(0,0,0);">
+    <section class="d-flex flex-column justify-content-between" id="home" style="background-image: url(&quot;/assets/img/space06.jpg&quot;);">
+        <div id="home-top"><a class="navbar-brand d-flex justify-content-center" href="/index.php" style="color: rgba(255,255,255,0.9);font-family: Montserrat, sans-serif;font-size: 28px;">ATOM</a>
+            <nav class="navbar navbar-light navbar-expand-md d-xl-flex align-items-xl-start">
+                <div class="container-fluid"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="nav navbar-nav mx-auto">
+                            <li class="nav-item" role="presentation"><a class="nav-link active" href="#" style="color: rgba(255,255,255);">HUBBLE</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="/falcon9.php" style="color: rgba(255,255,255);">FaLCon 9</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="/starlink.php" style="color: rgba(255,255,255);">STARLINK</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item" role="presentation"></li>
+                            <li class="nav-item" role="presentation"></li>
+                            <li class="nav-item" role="presentation"></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <h2 class="text-center" id="title" style="margin: 0px;padding: 0px;padding-top: 70px;">Téléscope</h2>
+            <h2 class="text-center" id="subtitle" style="padding-top: 15px;">Hubble</h2>
+        </div>
+        <div id="home-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 offset-2">
+                        <p style="font-style: italic;">"Embark on a Journey of Discovery"<br></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p id="p-top">&nbsp;57,60m</p>
+                        <p id="p-bot">Focale</p>
+                    </div>
+                    <div class="col bordure">
+                        <p id="p-top">2,40m</p>
+                        <p id="p-bot">Diamètre</p>
+                    </div>
+                    <div class="col">
+                        <p id="p-top">11 T</p>
+                        <p id="p-bot">Masse</p>
+                    </div>
+                    <div class="col align-self-center"><a class="btn btn-primary btn-block site-link" role="button" style="font-family: Montserrat, sans-serif;" href="https://hubblesite.org" target="_blank">En savoir plus</a></div>
+                </div>
+                <div class="row next-page">
+                    <div class="col"><button class="btn btn-link btn-block next-page" type="button" style="color: rgb(255,255,255);font-size: 32px;"><i class="icon ion-ios-arrow-down" href="#image-rocket"></i></button></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex align-items-center" id="image-rocket" style="background-image: url(&quot;/assets/img/space10.jpg&quot;);">
+        <div class="container" style="margin-top: 250px;">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <h3 style="color: rgb(255,255,255);">Hubble a été conçu pour découvrir l'univers</h3>
+                </div>
+                <div class="col">
+                    <p style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Hubble est un téléscope spatial développé par la NASA avec une participation de l'agence spatiale européenne, en orbite depuis 1990.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Start: Features Clean -->
+    <div class="features-clean" style="background-color: rgb(0,0,0);">
+        <div class="container" style="margin-top: 70px;">
+            <!-- Start: Intro -->
+            <div class="intro">
+                <h2 class="text-center" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);font-size: 28px;">Caractéristiques</h2>
+            </div>
+            <!-- End: Intro -->
+            <!-- Start: Features -->
+            <div class="row d-flex align-items-center features" style="color: rgb(255,255,255);">
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-ios-camera-outline icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Focale</h3>
+                    <p class="description" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);">590 km</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-arrow-resize icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Diamètre</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">24 m</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-ios-flag-outline icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Âge</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">29 ans</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-earth icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Masse</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">11 000 kg</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-ios-ionic-outline icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Altitude</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">590 km</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-ios-calendar-outline icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Fin de mission</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">2021</p>
+                </div>
+            </div>
+            <!-- End: Features -->
+        </div>
+    </div>
+    <!-- End: Features Clean -->
+    <!-- Start: Testimonials -->
+    <div class="testimonials-clean" style="background-color: rgb(0,0,0);margin-top: px;">
+        <div class="container">
+            <!-- Start: Intro -->
+            <div class="intro">
+                <h2 class="text-center" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;font-size: 28px;">Citations</h2>
+            </div>
+            <!-- End: Intro -->
+            <div class="row people">
+                <div class="col-md-6 col-lg-4 item">
+                    <div data-bs-hover-animate="pulse" class="box">
+                        <p class="description" style="font-family: Montserrat, sans-serif;"><em>"Personne n'avait encore pu observer l'impact d'une comète et la réponse d'une atmosphère à cette collision, et ce fut un tsunami cosmologique."</em><br></p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="/assets/img/1.jpg">
+                        <h5 class="name" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);">Roger-Maurice Bonnet</h5>
+                        <p class="title" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Astrophysicien français</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 item">
+                    <div data-bs-hover-animate="pulse" class="box">
+                        <p class="description" style="font-family: Montserrat, sans-serif;"><em>"Hubble a joué un rôle central. D'abord, parce que les images de collisions de galaxies qu'il a captées sont parmi les plus belles."</em><br></p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="/assets/img/3.jpg">
+                        <h5 class="name" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Pierre-Alain Duc</h5>
+                        <p class="title" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Chercheur astrophysicien du CEA</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 item">
+                    <div data-bs-hover-animate="pulse" class="box">
+                        <p class="description" style="font-family: Montserrat, sans-serif;font-style: italic;">"With this achievement, the estimated number of galaxies in the universe had multiplied enormously — to 50 billion, five times more than previously expected."<br></p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="/assets/img/2.jpg">
+                        <h5 class="name" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">John Noble Wilford</h5>
+                        <p class="title" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Journaliste Scientifique</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Testimonials -->
+    <section class="d-flex justify-content-center align-items-center" id="gallery">
+        <figure class="figure"><img class="img-fluid figure-img" src="/assets/img/space08.jpg" style="margin-top: 80px;width: 830px;height: 400px;">
+            <figcaption class="figure-caption"><span id="gallery-titre">Panorama</span>Photo récente de la Nébuleuse Carina</figcaption>
+        </figure>
+    </section>
+    
+    <!-- Start: Footer Basic -->
+    <div class="footer-basic" style="background-color: rgb(0,0,0);margin-top: 70px;">
+        <footer>
+            <!-- Start: Links -->
+            <ul class="list-inline d-flex justify-content-center">
+                <li class="list-inline-item"><a href="/index.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">ATOM</a></li>
+                <li class="list-inline-item" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;"><a href="/hubble.php">HUBBLE</a></li>
+                <li class="list-inline-item" style="color: rgb(255,255,255);"><a href="/falcon9.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">FALCON 9</a></li>
+                <li class="list-inline-item"><a href="/starlink.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">STARLINK</a></li>
+                <li class="list-inline-item"></li>
+            </ul>
+            <!-- End: Links -->
+            <!-- Start: Copyright -->
+            <p class="d-flex justify-content-center copyright" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Atom © 2020</p>
+            <!-- End: Copyright -->
+        </footer>
+    </div>
+    <!-- End: Footer Basic -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/bs-init.js"></script>
+    <script src="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.js"></script>
+</body>
+
+</html>
