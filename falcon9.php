011182001092bddff08ae6ff674e85049afdaf5a
GuigzSciencesU <60275318+GuigzSciencesU@users.noreply.github.com>
2020-02-25 09:25:11 +0100

    Create falcon9.php

---
+++ falcon9.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html style="background-color: #000000;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>ATOM</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="/assets/css/Contact-Form-Clean.css">
+    <link rel="stylesheet" href="/assets/css/Features-Clean.css">
+    <link rel="stylesheet" href="/assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.css">
+    <link rel="stylesheet" href="/assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="/assets/css/Newsletter-Subscription-Form.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/Testimonials.css">
+    <link rel="stylesheet" href="/assets/css/untitled.css">
+</head>
+
+<body style="background-color: rgb(0,0,0);">
+    <section class="d-flex flex-column justify-content-between" id="home" style="background-image: url(&quot;/assets/img/space11.png&quot;);">
+        <div id="home-top"><a class="navbar-brand d-flex justify-content-center" href="/index.php" style="color: rgba(255,255,255,0.9);font-family: Montserrat, sans-serif;font-size: 28px;">ATOM</a>
+            <nav class="navbar navbar-light navbar-expand-md d-xl-flex align-items-xl-start">
+                <div class="container-fluid"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="nav navbar-nav mx-auto">
+                            <li class="nav-item" role="presentation"><a class="nav-link active" href="/hubble.php" style="color: rgba(255,255,255);">HUBBLE</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="/falcon9.php" style="color: rgba(255,255,255);">FaLCon 9</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="/starlink.php" style="color: rgba(255,255,255);">STARLINK</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item" role="presentation"></li>
+                            <li class="nav-item" role="presentation"></li>
+                            <li class="nav-item" role="presentation"></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <h2 class="text-center" id="title" style="margin: 0px;padding: 0px;padding-top: 70px;">Fusée</h2>
+            <h2 class="text-center" id="subtitle" style="padding-top: 15px;">FALCON 9</h2>
+        </div>
+        <div id="home-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 offset-2">
+                        <p style="font-style: italic;">"Return Astronauts to space"<br></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p id="p-top">&nbsp;70m</p>
+                        <p id="p-bot">Taille</p>
+                    </div>
+                    <div class="col bordure">
+                        <p id="p-top">549 T</p>
+                        <p id="p-bot">Masse</p>
+                    </div>
+                    <div class="col">
+                        <p id="p-top">62 M€</p>
+                        <p id="p-bot">Prix</p>
+                    </div>
+                    <div class="col align-self-center"><a class="btn btn-primary btn-block site-link" role="button" style="font-family: Montserrat, sans-serif;" href="https://www.spacex.com/falcon9" target="_blank">En savoir plus</a></div>
+                </div>
+                <div class="row next-page">
+                    <div class="col"><button class="btn btn-link btn-block next-page" type="button" style="color: rgb(255,255,255);font-size: 32px;"><i class="icon ion-ios-arrow-down"></i></button></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex align-items-center" id="image-rocket" style="background-image: url(&quot;/assets/img/space13.png&quot;);width: 100%;height: 100vh;">
+        <div class="container" style="margin-top: 250px;">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <h3 style="color: rgb(255,255,255);">Avec Falcon 9, Space X souhaite pouvoir réutiliser des fusées.</h3>
+                </div>
+                <div class="col">
+                    <p style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Aussi, Falcon 9 est un moyen pour Space X de déployer des satelittes en orbite autour de la terre en toute sécurité.&nbsp;</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Start: Features Clean -->
+    <div class="features-clean" style="background-color: rgb(0,0,0);">
+        <div class="container" style="margin-top: 70px;">
+            <!-- Start: Intro -->
+            <div class="intro">
+                <h2 class="text-center" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);font-size: 28px;">Caractéristiques</h2>
+            </div>
+            <!-- End: Intro -->
+            <!-- Start: Features -->
+            <div class="row d-flex align-items-center features" style="color: rgb(255,255,255);">
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-plane icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Taille</h3>
+                    <p class="description" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);">70 m</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-arrow-resize icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Diamètre</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">3,7 m</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-ios-timer-outline icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Puissance</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">9 Moteurs</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-earth icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Mars</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">4020 Kg de disponible <br>pour un voyage vers Mars</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-ios-information icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Masse</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">549,054 Kg</p>
+                </div>
+                <div class="col-sm-6 col-lg-4 item"><i class="icon ion-pricetag icon" style="color: rgb(255,255,255);"></i>
+                    <h3 class="name">Prix</h3>
+                    <p class="description" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">62 M €</p>
+                </div>
+            </div>
+            <!-- End: Features -->
+        </div>
+    </div>
+    <!-- End: Features Clean -->
+    <!-- Start: Testimonials -->
+    <div class="testimonials-clean" style="background-color: rgb(0,0,0);margin-top: px;">
+        <div class="container">
+            <!-- Start: Intro -->
+            <div class="intro">
+                <h2 class="text-center" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;font-size: 28px;">Citations</h2>
+            </div>
+            <!-- End: Intro -->
+            <div class="row people">
+                <div class="col-md-6 col-lg-4 item">
+                    <div data-bs-hover-animate="pulse" class="box">
+                        <p class="description" style="font-family: Montserrat, sans-serif;font-style: italic;">"Space X, un lancement de nuit époustouflant à revivre en images".</p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="/assets/img/1.jpg">
+                        <h5 class="name" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);">Floriane Boyer</h5>
+                        <p class="title" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Rédactrice</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 item">
+                    <div data-bs-hover-animate="pulse" class="box">
+                        <p class="description" style="font-family: Montserrat, sans-serif;font-style: italic;">"Space X prouve le recyclage de ses fusées avec le dernier Starlink".</p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="/assets/img/3.jpg">
+                        <h5 class="name" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Félix Gouty</h5>
+                        <p class="title" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Vulgarisateur Scientifique</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 item">
+                    <div data-bs-hover-animate="pulse" class="box">
+                        <p class="description" style="font-family: Montserrat, sans-serif;font-style: italic;">"C'est irréel !"</p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="/assets/img/2.jpg">
+                        <h5 class="name" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Elon Musk</h5>
+                        <p class="title" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Fondateur de Space X</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Testimonials -->
+    <section class="d-flex justify-content-center align-items-center" id="gallery">
+        <figure class="figure"><img class="img-fluid figure-img" src="/assets/img/space12.jpg" style="margin-top: 80px;height: 400px;width: 830px;">
+            <figcaption class="figure-caption"><span id="gallery-titre">Panorama</span>Lancement d'une fusée Falcon 9</figcaption>
+        </figure>
+    </section>
+   
+    <!-- Start: Footer Basic -->
+    <div class="footer-basic" style="background-color: rgb(0,0,0);margin-top: 70px;">
+        <footer>
+            <!-- Start: Links -->
+            <ul class="list-inline d-flex justify-content-center">
+                <li class="list-inline-item"><a href="/index.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">ATOM</a></li>
+                <li class="list-inline-item" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;"><a href="/hubble.php">HUBBLE</a></li>
+                <li class="list-inline-item" style="color: rgb(255,255,255);"><a href="/falcon9.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">FALCON 9</a></li>
+                <li class="list-inline-item"><a href="/starlink.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">STARLINK</a></li>
+                <li class="list-inline-item"></li>
+            </ul>
+            <!-- End: Links -->
+            <!-- Start: Copyright -->
+            <p class="d-flex justify-content-center copyright" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;">Atom © 2020</p>
+            <!-- End: Copyright -->
+        </footer>
+    </div>
+    <!-- End: Footer Basic -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/bs-init.js"></script>
+    <script src="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.js"></script>
+</body>
+
+</html>
