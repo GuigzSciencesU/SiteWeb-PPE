7774a047096588c8ecf0efdc3e87c999c81e1374
GuigzSciencesU <60275318+GuigzSciencesU@users.noreply.github.com>
2020-02-25 09:22:27 +0100

    Update indexUS.php

--- indexUS.php
+++ indexUS.php
@@ -1,40 +1,84 @@
 <!DOCTYPE html>
-<html>
+<html style="background-color: #000000;">
 
+<!-- #Insertion des liens css/fonts/icones -->
 
-      <title>U'Sports</title>
-
-<!-- Import Materialize CSS / Icones -->
 <head>
-
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="css/custom_us.css"  media="screen,projection"/>
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>ATOM</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="/assets/css/Contact-Form-Clean.css">
+    <link rel="stylesheet" href="/assets/css/Features-Clean.css">
+    <link rel="stylesheet" href="/assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.css">
+    <link rel="stylesheet" href="/assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="/assets/css/Newsletter-Subscription-Form.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/Testimonials.css">
+    <link rel="stylesheet" href="/assets/css/untitled.css">
+    <link rel="stylesheet" type="text/css" href="/assets/img/ATOM.png">
 </head>
 
 
-<!-- Import Contenu site -->
-<body>
+<!-- #Construction du Menu principal avec Titre -->
+<!-- #Appel des autres pages html -->
 
-      <!-- Header -->
-      <?php include "header_us.php" ?>
+<body style="background-color: rgb(0,0,0);">
+    <section class="d-flex flex-column justify-content-between" id="home" style="background-image: url(&quot;/assets/img/space16.jpg&quot;);">
+        <div id="home-top">
+            <nav class="navbar navbar-light navbar-expand-md d-xl-flex align-items-xl-start">
+                <div class="container-fluid"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="nav navbar-nav mx-auto">
+                            <li class="nav-item" role="presentation"></li>
+                            <li class="nav-item" role="presentation"></li>
+                            <li class="nav-item" role="presentation"></li>
+                        </ul>
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item" role="presentation"></li>
+                            <li class="nav-item" role="presentation"></li>
+                            <li class="nav-item" role="presentation"></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <h2 class="text-center" id="title" style="margin: 0px;padding: 0px;padding-top: 70px;"></h2>
+            <h2 class="text-center" id="subtitle" style="padding-top: 15px;">ATOM</h2>
+        </div>
+        <div id="home-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <p id="p-top">&nbsp;<a data-bs-hover-animate="flash" href="/hubble.php" style="color: rgba(255,255,255);"></a><a class="navbar-brand" data-bs-hover-animate="bounce" href="/hubble.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;font-size: 26px;font-weight: bold;">&nbsp; HUBBLE</a></p>
+                        <p
+                            id="p-bot">NASA</p>
+                    </div>
+                    <div class="col bordure">
+                        <p id="p-top"><a class="navbar-brand" data-bs-hover-animate="bounce" href="/falcon9.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;font-size: 26px;font-weight: bold;">&nbsp; FALCON 9</a></p>
+                        <p id="p-bot">Space X</p>
+                    </div>
+                    <div class="col">
+                        <p id="p-top"><a class="navbar-brand" data-bs-hover-animate="bounce" href="/starlink.php" style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;font-size: 26px;font-weight: bold;">&nbsp; STARLINK</a></p>
+                        <p id="p-bot">Space X</p>
+                    </div>
+                </div>
+                <div class="row next-page">
+                    <div class="col"><button class="btn btn-link btn-block next-page" type="button" style="color: rgb(255,255,255);font-size: 32px;"><i class="icon ion-minus-round" href=""></i></button></div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-      <!-- Bloc A -->
-      <?php include "parallax_us.php" ?>
+<!-- #Exécution des scripts JS -->
 
-      <!-- Footer -->
-      <?php include "footer_us.php" ?>
-
-      <!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/jquery.min.js"></script>      
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-      <script type="text/javascript" src="js/custom_us.js"></script>
-
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/bs-init.js"></script>
+    <script src="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.js"></script>
+</body>
 
 </html>
